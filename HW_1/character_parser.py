5863c63050568e0e6ca5d28c8efaf2ef8e033a73
Ben Johnson <Ben@dhcp-10-134-194-184.uawifi.arizona.edu>
2016-01-21 11:32:45 -0700

    Finished python script for CharParser

--- HW_1/character_parser.py
+++ HW_1/character_parser.py
@@ -2,12 +2,32 @@ import os
 
 if __name__ == "__main__":
     fileExt = str(os.getcwd()) + "/characters.txt"
+    newExt = str(os.getcwd()) + "/testCharFile.html"
     myfile = open((fileExt),'r')
+    writeFile = open((newExt),'w')
+
+    writeFile.write("<table>\n<tr>")
+    writeFile.write("<th><b>Character</b></th>")
+    writeFile.write("<th><b>Description</b></th>")
+    writeFile.write("<th><b>Image</b></th>")
+    writeFile.write("</tr>")
+
     for line in myfile:
-        seperate = line.split(" - ", 1)
-        for word in seperate:
-            print (str("<td>" + word + "</td>"))
-            #get rid of the newline after the last word
-        print ("<td>IMAGELOC</td>")
-        print ()
+        try:
+            writeFile.write("<tr>")
+            seperate = line.split(" - ", 1)
+            fix = seperate[1].split("\n",1)
+            line1 = ("<td>" + seperate[0] + "</td>\n")
+            line2 = ("<td>" + fix[0] + "</td>\n")
+            line3 = ("<td>IMAGELOC</td>")
+            writeFile.write(line1)
+            writeFile.write(line2)
+            writeFile.write(line3)
+            writeFile.write("</tr>")
+
+        except:
+            writeFile.write("\n\n")
+    writeFile.write("</table>")
+    myfile.close()
+    writeFile.close()
 
