e74edaaf72129e75861d943080aa49858a045ffa
Ben Johnson <Ben Johnson>
2016-01-25 20:01:54 -0700

    Finished Character.html file and working on the dream file.

--- HW_1/character_parser.py
+++ HW_1/character_parser.py
@@ -5,29 +5,29 @@ if __name__ == "__main__":
     newExt = str(os.getcwd()) + "/testCharFile.html"
     myfile = open((fileExt),'r')
     writeFile = open((newExt),'w')
-
-    writeFile.write("<table>\n<tr>")
-    writeFile.write("<th><b>Character</b></th>")
-    writeFile.write("<th><b>Description</b></th>")
-    writeFile.write("<th><b>Image</b></th>")
-    writeFile.write("</tr>")
+    writeFile.write(str("<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">"))
+    writeFile.write("\n<table>\n\t<tr>")
+    writeFile.write("\n\t\t<th>Character</th>")
+    writeFile.write("\n\t\t<th>Description</th>")
+    writeFile.write("\n\t\t<th>Image</th>")
+    writeFile.write("\n\t</tr>")
 
     for line in myfile:
         try:
-            writeFile.write("<tr>")
             seperate = line.split(" - ", 1)
             fix = seperate[1].split("\n",1)
-            line1 = ("<td>" + seperate[0] + "</td>\n")
-            line2 = ("<td>" + fix[0] + "</td>\n")
-            line3 = ("<td>IMAGELOC</td>")
+            writeFile.write("\n\t<tr>")
+            line1 = ("\n\t\t<td>" + seperate[0] + "</td>")
+            line2 = ("\n\t\t<td>" + fix[0] + "</td>")
+            line3 = ("\n\t\t<td>IMAGELOC</td>")
             writeFile.write(line1)
             writeFile.write(line2)
             writeFile.write(line3)
-            writeFile.write("</tr>")
+            writeFile.write("\n\t</tr>")
 
         except:
-            writeFile.write("\n\n")
-    writeFile.write("</table>")
+            print("error?")
+    writeFile.write("\n</table>")
     myfile.close()
     writeFile.close()
 
